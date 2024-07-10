1d3d691814595c06c96b765ae9f20709942a94a3
Joey Smith <jsmith@webinertia.net>
2024-07-10 02:47:21 -0500

    Begins work on porting index.template.php into the layout.phtml file. Adds two test cases that are currently only stubs. Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- test/Actions/AbstractActionTest.php
+++ test/Actions/AbstractActionTest.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace SmfTest\App\Actions;
 
+use App\Actions\AbstractAction;
+
 class AbstractActionTest extends AbstractActionTestCase
 {
-
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
 }
